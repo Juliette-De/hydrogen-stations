286118f17ea27d0f5a80492f2cc03eb2b506f526
Juliette-De <115700542+Juliette-De@users.noreply.github.com>
2023-03-16 13:47:59 +0100

    Update 1_Market_Sizing.py

--- webapp_final/pages/1_Market_Sizing.py
+++ webapp_final/pages/1_Market_Sizing.py
@@ -89,16 +89,19 @@ bar_plot_per_region.add_trace(go.Bar(x=df_all_stations.index,
                                      y=df_all_stations["count_optimistic"],
                                      name="Optimistic",
                                      marker_color=df_all_stations.label.map({"hub": "blue", "road": "lightblue"}),
+                                     legendgroup=1,
                                      legendgrouptitle="Optimistic"))
 bar_plot_per_region.add_trace(go.Bar(x=df_all_stations.index,
                                      y=df_all_stations["count_moderate"],
                                      name="Moderate",
                                      marker_color=df_all_stations.label.map({"hub": "green", "road": "lightgreen"}),
+                                     legendgroup=2,
                                      legendgrouptitle="Moderate"))
 bar_plot_per_region.add_trace(go.Bar(x=df_all_stations.index,
                                      y=df_all_stations["count_conservative"],
                                      name="Conservative",
                                      marker_color=df_all_stations.label.map({"hub": "yellow", "road": "lightyellow"}),
+                                     legendgroup=3,
                                      legendgrouptitle="Conservative"))
 
 bar_plot_per_region.update_layout(template="plotly", barmode="group", xaxis_tickangle=-45)
