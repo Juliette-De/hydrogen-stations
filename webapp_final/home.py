160f90aed671eac07c7b312ad1fd58f609d60dae
Juliette-De <115700542+Juliette-De@users.noreply.github.com>
2023-03-16 02:21:30 +0100

    Update home.py

--- webapp_final/home.py
+++ webapp_final/home.py
@@ -6,14 +6,13 @@ It offers the following three features, each on one page:
 * a sizing of the market according to the scenario envisaged - optimistic, moderate or conservative (see the slides for more information), including:
   * the number of stations,
   * their distribution by region;
-- the location and size of the stations;
-- a dashboard of the profitability of each station, according to the following competitive scenarios:
-        - 1: A single network in France
-        - 2: Two players entering the market simultaneously (Air Liquide in blue and Red Team in red)
-        - 3: Air Liquide arriving after an incumbent operator transformed its network of oil stations into H2.""")
+* the location and size of the stations;
+* a dashboard of the profitability of each station, according to the following competitive scenarios:
+  * 1: A single network in France
+  * 2: Two players entering the market simultaneously (Air Liquide in blue and Red Team in red)
+  * 3: Air Liquide arriving after an incumbent operator transformed its network of oil stations into H2.""")
         
-# def app():
-    
+# def app(): 
 
 # if __name__ == "__app__":
 #     app()
