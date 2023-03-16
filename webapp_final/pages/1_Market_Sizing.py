a505ee3b008da9ec344cafd89e897c7f687fb4b5
Juliette-De <115700542+Juliette-De@users.noreply.github.com>
2023-03-16 13:40:33 +0100

    Update 1_Market_Sizing.py

--- webapp_final/pages/1_Market_Sizing.py
+++ webapp_final/pages/1_Market_Sizing.py
@@ -85,32 +85,21 @@ with col3:
 ## Plot
 df_all_stations.sort_index().sort_values(by="label", inplace=True)
 bar_plot_per_region = go.Figure()
-bar_plot_per_region.add_trace(
-    go.Bar(x=df_all_stations.index,
-           y=df_all_stations["count_optimistic"],
-           name="Optimistic",
-           marker_color=df_all_stations.label.map({"hub": "blue", "road": "lightblue"}),
-           legendgroup="Optimistic"
-    )
-)
-bar_plot_per_region.add_trace(
-    go.Bar(x=df_all_stations.index,
-           y=df_all_stations["count_moderate"],
-           name="Moderate",
-           marker_color=df_all_stations.label.map({"hub": "green", "road": "lightgreen"}),
-           legendgroup="Moderate"
-    )
-)
-bar_plot_per_region.add_trace(
-    go.Bar(x=df_all_stations.index,
-           y=df_all_stations["count_conservative"],
-           name="Conservative",
-           marker_color=df_all_stations.label.map({"hub": "yellow", "road": "lightyellow"}),
-           legendgroup="Conservative")
-    )
-)
+bar_plot_per_region.add_trace(go.Bar(x=df_all_stations.index,
+                                     y=df_all_stations["count_optimistic"],
+                                     name="Optimistic",
+                                     marker_color=df_all_stations.label.map({"hub": "blue", "road": "lightblue"}),
+                                     legendgroup="Optimistic"))
+bar_plot_per_region.add_trace(go.Bar(x=df_all_stations.index,
+                                     y=df_all_stations["count_moderate"],
+                                     name="Moderate",
+                                     marker_color=df_all_stations.label.map({"hub": "green", "road": "lightgreen"}),
+                                     legendgroup="Moderate"))
+bar_plot_per_region.add_trace(go.Bar(x=df_all_stations.index,
+                                     y=df_all_stations["count_conservative"],
+                                     name="Conservative",
+                                     marker_color=df_all_stations.label.map({"hub": "yellow", "road": "lightyellow"}),
+                                     legendgroup="Conservative"))
 
-bar_plot_per_region.update_layout(
-    template="plotly", barmode="group", xaxis_tickangle=-45
-)
+bar_plot_per_region.update_layout(template="plotly", barmode="group", xaxis_tickangle=-45)
 st.plotly_chart(bar_plot_per_region)
