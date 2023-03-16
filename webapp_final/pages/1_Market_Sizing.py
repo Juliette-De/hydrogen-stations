df941aef49816b4795c7654d717e15e7f7b462f1
Juliette-De <115700542+Juliette-De@users.noreply.github.com>
2023-03-16 17:11:06 +0100

    Update 1_Market_Sizing.py

--- webapp_final/pages/1_Market_Sizing.py
+++ webapp_final/pages/1_Market_Sizing.py
@@ -127,8 +127,14 @@ st.plotly_chart(fig)
 st.dataframe(df_all_stations.melt(var_name='count_', value_name='stations', id_vars='label', ignore_index=False))
 
 
-fig = go.Figure(data=[go.Bar(name='Optimistic', x=df_all_stations.index, y=df_all_stations.count_optimistic, marker_color='label'),
-                      go.Bar(name='Moderate', x=df_all_stations.index, y=df_all_stations.count_moderate, marker_color='label'),
+fig = go.Figure(data=[go.Bar(name='Optimistic',
+                             x=df_all_stations.index,
+                             y=df_all_stations.count_optimistic,
+                             marker_color=df_all_stations.label.map({"hub": "yellow", "road": "lightyellow"})),
+                      go.Bar(name='Moderate',
+                             x=df_all_stations.index,
+                             y=df_all_stations.count_moderate,
+                             marker_color=df_all_stations.label.map({"hub": "yellow", "road": "lightyellow"})),
                       go.Bar(name='Conservative', x=df_all_stations.index, y=df_all_stations.count_conservative, marker_color='label')
 ])
 fig.update_layout(barmode='group')
