afc36fed461b5d68a5c9a57c0ae0add0df94ba88
Juliette-De <115700542+Juliette-De@users.noreply.github.com>
2023-03-16 18:07:16 +0100

    Update 1_Market_Sizing.py

--- webapp_final/pages/1_Market_Sizing.py
+++ webapp_final/pages/1_Market_Sizing.py
@@ -106,11 +106,9 @@ fig.update_yaxes(title_text = "Number of stations (near hubs + along roads)") # 
 st.plotly_chart(fig)
 
 
-st.dataframe(df_all_stations)
-
 regions = df_all_stations.groupby(['region_name']).sum().rename(columns={"count_optimistic": "Optimistic",
                                                                          "count_moderate": "Moderate",
                                                                          "count_conservative": "Conservative"})
-regions.loc['Total'] = df.sum(numeric_only=True)
+regions.loc['Total'] = regions.sum(numeric_only=True)
 
 st.dataframe(regions)
