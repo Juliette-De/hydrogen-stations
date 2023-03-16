c64267cc9989eb960026b129337f956b7d5f8ed3
Juliette-De <115700542+Juliette-De@users.noreply.github.com>
2023-03-16 02:20:57 +0100

    Update home.py

--- webapp_final/home.py
+++ webapp_final/home.py
@@ -3,9 +3,9 @@ from streamlit_folium import folium_static
 
 st.write("""This application has been developed to visualize the locations where to implement hydrogen stations in 2030 and 2040, according to different parameters and scenarios.
 It offers the following three features, each on one page:
-- a sizing of the market according to the scenario envisaged - optimistic, moderate or conservative (see the slides for more information), including:
-        - the number of stations,
-        - their distribution by region;
+* a sizing of the market according to the scenario envisaged - optimistic, moderate or conservative (see the slides for more information), including:
+  * the number of stations,
+  * their distribution by region;
 - the location and size of the stations;
 - a dashboard of the profitability of each station, according to the following competitive scenarios:
         - 1: A single network in France
